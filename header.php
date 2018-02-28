6ebdccdaa865b7336833ea9fbf38624242b5884a
Dan <ds5ct@virginia.edu>
2018-02-28 14:53:13 -0500

    Refactor

--- header.php
+++ header.php
@@ -1,18 +1,17 @@
-<!-- Header -->
-	<header id="header">
-		<div class="inner">
+<header id="header">
+	<div class="inner">
 
-			<!-- Logo -->
-				<a href="index.php" class="logo">
-					<span class="symbol"><img src="images/logo.svg" alt="" /></span><span class="title">Climbing Member Management Tool</span>
-				</a>
+		<!-- Logo -->
+			<a href="index.php" class="logo">
+				<span class="symbol"><img src="images/logo.svg" alt="" /></span><span class="title">Climbing Member Management Tool</span>
+			</a>
 
-			<!-- Nav -->
-				<nav>
-					<ul>
-						<li><a href="#menu">Menu</a></li>
-					</ul>
-				</nav>
+		<!-- Nav -->
+			<nav>
+				<ul>
+					<li><a href="#menu">Menu</a></li>
+				</ul>
+			</nav>
 
-		</div>
-	</header>
+	</div>
+</header>
---
+++ menu.php
@@ -0,0 +1,9 @@
+<nav id="menu">
+	<h2>Menu</h2>
+	<ul>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="userportal.php">User Portal</a></li>
+		<li><a href="usercheckin.php">User Checkin</a></li>
+		<li><a href="staffportal.php">Staff Portal</a></li>
+	</ul>
+</nav>
